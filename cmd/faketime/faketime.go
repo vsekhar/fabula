8c2c38b9aa92ededa62d6586ad60ba177499a6b4
Vivek Sekhar <vivek@viveksekhar.ca>
2019-07-30 20:45:28 -0400

    Commit wait

--- cmd/faketime/faketime.go
+++ cmd/faketime/faketime.go
@@ -14,32 +14,56 @@ import (
 var port = flag.Int("port", 8080, "port to listen on (default 8080)")
 var jitterMs = flag.Float64("jitter", 10.0, "stddev of timestamp range in milliseconds (default 10.0)")
 
-type fakeTime struct{}
+type interval struct {
+	earliest, latest int64
+}
 
-func (*fakeTime) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// epislon returns a random duration drawn from a normal distribution with
+// mean 0 and standard deviation *jitterMs (in milliseconds)
+func epsilon() time.Duration {
+	return time.Duration(rand.NormFloat64() * *jitterMs)
+}
+
+func ttNow() (earliest, latest time.Time) {
+	t := time.Now()
+	e := epsilon()
+	return t.Add(e), t.Add(-e)
+}
+
+func wait() time.Time {
+	t := time.Now()
+	time.Sleep(epsilon() * time.Millisecond)
+	return t
+}
+
+func handleNow(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	if r.URL.Path != "/.well-known/truetime/now" {
-		w.WriteHeader(http.StatusNotFound)
+	earliest, latest := ttNow()
+	w.Header().Add("Consistent-Range-Earliest", fmt.Sprintf("%d", earliest.UnixNano()/1000))
+	w.Header().Add("Consistent-Range-Latest", fmt.Sprintf("%d", latest.UnixNano()/1000))
+	// TODO: signature
+}
+
+func handleCommitWait(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	midPointMs := time.Now().UnixNano() / 1000
-	epsilon := int64(rand.NormFloat64() * *jitterMs)
-	earliest := midPointMs - epsilon
-	latest := midPointMs + epsilon
-	h := w.Header()
-	h.Add("Consistent-Range-Earliest", fmt.Sprintf("%d", earliest))
-	h.Add("Consistent-Range-Latest", fmt.Sprintf("%d", latest))
-	// TODO: signature
+	w.Header().Add("Consistent-Timestamp", fmt.Sprintf("%d", wait().UnixNano()/1000))
 }
 
 func main() {
 	flag.Parse()
+	m := http.NewServeMux()
+	m.HandleFunc("/.well-known/truetime/now", handleNow)
+	m.HandleFunc("/.well-known/truetime/commitwait", handleCommitWait)
 	s := &http.Server{
 		Addr:    fmt.Sprintf(":%d", *port),
-		Handler: new(fakeTime),
+		Handler: m,
+		// TODO: TLSConfig
 	}
 	log.Fatal(s.ListenAndServe())
 }

17949c79e8c07e2790bc12f9669ad4879740ce56
Vivek Sekhar <vivek@viveksekhar.ca>
2019-08-07 15:38:56 -0400

    Hacking

--- cmd/commitclient/commitclient.go
+++ cmd/commitclient/commitclient.go
@@ -3,18 +3,24 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 )
 
-var timeServer = flag.String("timeserver", "", "address:port of timeserver")
-var server1 = flag.String("server1", "", "address:port of first server")
-var server2 = flag.String("server2", "", "address:port of second server")
+var timeServer = flag.String("timeserver", "", "address of timeserver (e.g. 'timeserver.com'")
+var server1 = flag.String("server1", "", "address of first server (e.g. 'dataserver1.com'")
+var server2 = flag.String("server2", "", "address of second server (e.g. 'dataserver2.com'")
 
 func main() {
 	flag.Parse()
+	log.SetFlags(log.LstdFlags)
 	if *server1 == "" {
 		log.Fatal("No server1 specified")
 	}
 	if *server2 == "" {
 		log.Fatal("No server2 specified")
+	}
+	_, err := http.Get("http://" + *server1)
+	if err != nil {
+		log.Fatal(err)
 	}
 }

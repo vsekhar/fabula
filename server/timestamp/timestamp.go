43e1a50d71e474bec69525eef445f67906473cf1
Vivek Sekhar <vivek@viveksekhar.ca>
2020-08-11 18:21:33 -0400

    Canonical time serialization

--- server/timestamp/timestamp.go
+++ server/timestamp/timestamp.go
@@ -1,4 +1,4 @@
-// Package timestamp handles converting Timestamp values to canonical string
+// Package timestamp handles converting time.Time values to unambiguous string
 // and byte representations.
 package timestamp
 
@@ -9,27 +9,28 @@ import (
 	"time"
 )
 
-// toString encodes a time.Time object to a string containing an integer number
+// ToString encodes a time.Time object to a string containing an integer number
 // of UTC nanoseconds from the Unix epoch.
-func toString(t time.Time) string {
-	return fmt.Sprintf("%d", t.UnixNano())
+func ToString(t time.Time) string {
+	n := t.UnixNano()
+	return fmt.Sprintf("%d", n)
 }
 
-// fromString decodes a time.Time object from a string containing an integer
+// FromString decodes a time.Time object from a string containing an integer
 // number of UTC nanoseconds from the Unix epoch.
-func fromString(t string) (time.Time, error) {
+func FromString(t string) (time.Time, error) {
 	nanos, err := strconv.ParseInt(t, 10, 64)
 	if err != nil {
 		return time.Time{}, err
 	}
-	return time.Unix(0, nanos), nil
+	return time.Unix(0, nanos).In(time.UTC), nil
 }
 
 // MaxLen is the maximum number of bytes a Timestamp will occupy when converted
 // to a binary representation.
 const MaxLen = binary.MaxVarintLen64
 
-// toBytes encodes a time.Time object to a little-endian binary integer number
+// ToBytes encodes a time.Time object to a little-endian binary integer number
 // of UTC nanoseconds from the Unix epoch, writes the result to a buffer and
 // returns the number of bytes written. toBytes panics if the buffer is too
 // small.
@@ -37,13 +38,13 @@ const MaxLen = binary.MaxVarintLen64
 // A variable-length encoding is used; smaller values require fewer bytes. For a
 // specification, see
 // https://developers.google.com/protocol-buffers/docs/encoding.
-func toBytes(buf []byte, t time.Time) int {
+func ToBytes(buf []byte, t time.Time) int {
 	nanos := t.UnixNano()
 	n := binary.PutVarint(buf, nanos) // panics if b is too small
 	return n
 }
 
-// fromBytes decodes a time.Time object from buf and returns the object and the
+// FromBytes decodes a time.Time object from buf and returns the object and the
 // number of bytes read (> 0). If an error occurred, the value is time.Time(0)
 // and the number of bytes n is <= 0 with the following meaning:
 //
@@ -54,8 +55,8 @@ func toBytes(buf []byte, t time.Time) int {
 // A variable-length encoding is expected; smaller values require fewer bytes.
 // For a specification, see
 // https://developers.google.com/protocol-buffers/docs/encoding.
-func fromBytes(buf []byte) (time.Time, int) {
-	nanos, n := binary.Varint(b)
+func FromBytes(buf []byte) (time.Time, int) {
+	nanos, n := binary.Varint(buf)
 	t := time.Unix(0, nanos)
 	return t, n
 }
---
+++ server/timestamp/timestamp_test.go
@@ -0,0 +1,71 @@
+package timestamp_test
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/vsekhar/fabula/server/timestamp"
+)
+
+var stringCases map[time.Time]string = map[time.Time]string{
+	time.Unix(0, 0): "0",
+	time.Date(1970, 1, 1, 0, 0, 0, 1, time.UTC): "1",
+	time.Unix(0, 500): "500",
+	time.Unix(1, 0):   "1000000000",
+	time.Date(1970, 1, 1, 0, 0, 1, 1, time.UTC):                          "1000000001",
+	time.Date(1969, 12, 30, 0, 0, 0, 0, time.UTC):                        "-172800000000000",
+	time.Date(1720, 12, 30, 0, 0, 0, 0, time.UTC):                        "-7857820800000000000",
+	time.Date(2020, 8, 11, 18, 01, 34, 58982, time.FixedZone("test", 7)): "1597168887000058982",
+
+	// These don't work: https://github.com/golang/go/issues/40702
+	// "-6795364578871345151": time.Date(1, 1, 1, 0, 0, 0, 1, time.UTC),
+	// "-6795364578871345152": time.Time{},
+	// "-6795364578871245152": time.Time{}.Add(100 * time.Microsecond),
+}
+
+func TestToString(t *testing.T) {
+	for v, s := range stringCases {
+		if g := timestamp.ToString(v); g != s {
+			t.Errorf("bad conversion to string of %s: expected '%s', got '%s'", v, s, g)
+		}
+	}
+}
+
+func TestFromString(t *testing.T) {
+	fmt.Printf("epoch: %d", time.Unix(0, 0).Sub(time.Time{}).Nanoseconds())
+	for v, s := range stringCases {
+		if g, err := timestamp.FromString(s); !g.Equal(v) {
+			t.Errorf("bad conversion from string of %s: expected '%s', got '%s' (err: %s)", s, v, g, err)
+		}
+	}
+}
+
+var binaryCases map[time.Time][]byte = map[time.Time][]byte{
+	time.Unix(0, 0): {0},
+	time.Date(1970, 1, 1, 0, 0, 0, 1, time.UTC): {2},
+	time.Unix(0, 500): {232, 7},
+	time.Unix(1, 0):   {128, 168, 214, 185, 7},
+	time.Date(1970, 1, 1, 0, 0, 1, 1, time.UTC):                          {130, 168, 214, 185, 7},
+	time.Date(1969, 12, 30, 0, 0, 0, 0, time.UTC):                        {255, 255, 239, 169, 164, 202, 78},
+	time.Date(1720, 12, 30, 0, 0, 0, 0, time.UTC):                        {255, 255, 231, 202, 210, 152, 203, 140, 218, 1},
+	time.Date(2020, 8, 11, 18, 01, 34, 58982, time.FixedZone("test", 7)): {204, 177, 142, 191, 255, 168, 164, 170, 44},
+}
+
+func TestToBytes(t *testing.T) {
+	buf := make([]byte, 32)
+	for v, b := range binaryCases {
+		if n := timestamp.ToBytes(buf, v); !bytes.Equal(buf[:n], b) {
+			t.Errorf("bad conversion to bytes of %s: expected '%x', got '%v'", v, b, buf[:n])
+		}
+	}
+}
+
+func TestFromBytes(t *testing.T) {
+	for v, b := range binaryCases {
+		if g, _ := timestamp.FromBytes(b); !g.Equal(v) {
+			t.Errorf("bad conversion from bytes of %v: expected '%s', got '%s'", b, v, g)
+		}
+	}
+}

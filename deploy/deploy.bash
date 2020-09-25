df233354281f87614900219ff9eac0649a865ab8
Vivek Sekhar <vivek@viveksekhar.ca>
2020-09-25 01:01:11 -0400

    Fix build for non-standard Dockerfile location

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -8,10 +8,13 @@ PROJECT=$(gcloud config get-value project)
 SERVICE=$NAME
 IMAGE=$NAME-image
 
+# TODO: GCS bucket
 
 # server in us-central1
-(cd server && exec go mod tidy)
-gcloud builds submit server --tag gcr.io/$PROJECT/$IMAGE
+gcloud builds submit $(git rev-parse --show-toplevel) \
+    --config server/cloudbuild.yaml \
+    --substitutions=_IMAGE="$IMAGE"
+
 gcloud run deploy $SERVICE \
     --image gcr.io/$PROJECT/$IMAGE \
     --set-env-vars=DB=$DB_STRING \

edcdc3f561a5a50ebcc1b73e9b1b28491b764f1a
Vivek Sekhar <vivek@viveksekhar.ca>
2020-09-25 01:00:26 -0400

    Clean up spanner stuff

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -7,20 +7,7 @@ NAME=fabula
 PROJECT=$(gcloud config get-value project)
 SERVICE=$NAME
 IMAGE=$NAME-image
-SPANNER_INSTANCE=$NAME-spanner-instance
-SPANNER_DB=$NAME-spanner-db
-DB_STRING=projects/$PROJECT/instances/$SPANNER_INSTANCE/databases/$SPANNER_DB
 
-# spanner
-gcloud deployment-manager deployments update $NAME --config dmconfig.yaml || \
-gcloud deployment-manager deployments create $NAME --config dmconfig.yaml
-
-gcloud spanner databases create $SPANNER_DB --instance=$SPANNER_INSTANCE || true
-
-gcloud spanner databases ddl update $SPANNER_DB \
-    --instance=$SPANNER_INSTANCE \
-    --ddl="$(cat storage/spanner.sdl)" || \
-    read -p "Press Ctrl-C to cancel or any key to continue..."
 
 # server in us-central1
 (cd server && exec go mod tidy)

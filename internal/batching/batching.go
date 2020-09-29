8eef8ec4cdbdda45de531ad6f0f80a37defc85a5
Vivek Sekhar <vivek@viveksekhar.ca>
2020-09-29 19:01:48 -0400

    Drop batch buffer, start timer with first item.

--- internal/batching/batching.go
+++ internal/batching/batching.go
@@ -6,35 +6,41 @@ import (
 	"time"
 )
 
-const batchBuffer = 100
-
 // Channel returns a channel that will produce batches of items at most
 // batchSize in number and with no item waiting longer than maxWait.
 //
 // When finished, the caller should close rch. BatchChannel will then close the
 // returned channel after the last batch is processed.
 func Channel(rch <-chan interface{}, batchSize int, maxWait time.Duration) <-chan []interface{} {
-	batches := make(chan []interface{}, batchBuffer)
+	batches := make(chan []interface{})
 	go func() {
 		var items []interface{}
-		delayTimer := time.NewTimer(maxWait)
+		t := time.NewTimer(maxWait)
+		if !t.Stop() {
+			<-t.C
+		}
 		for {
 			select {
-			case <-delayTimer.C:
-				if len(items) > 0 {
-					batches <- items
-					items = nil
-				}
-				delayTimer.Reset(maxWait)
+			case <-t.C:
+				t.Stop()
+				batches <- items
+				items = nil
 			case n, ok := <-rch:
 				if ok {
+					// Received new item
 					items = append(items, n)
+					if len(items) == 1 {
+						t.Reset(maxWait)
+					}
 					if len(items) == batchSize {
 						batches <- items
 						items = nil
-						delayTimer.Reset(maxWait)
+						if !t.Stop() {
+							<-t.C
+						}
 					}
 				} else {
+					// Channel closed
 					if len(items) > 0 {
 						batches <- items
 						items = nil

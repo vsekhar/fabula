df7a7ed251d9b3d6ac19c4d0aebe8c6687c5dbf2
Vivek Sekhar <vivek@viveksekhar.ca>
2020-10-07 04:21:32 -0400

    Cleanup

--- pkg/sortablebase64/sortablebase64.go
+++ pkg/sortablebase64/sortablebase64.go
@@ -44,42 +44,26 @@ func init() {
 // The returned string is always 11 bytes in length.
 func EncodeUint64(n uint64) string {
 	rb := strings.Builder{}
-
-	// Write big-endian
 	for i := 0; i < 11; i++ {
-		rb.WriteByte(Alphabet[(n>>(60-(i*6)))&0x3F])
+		rb.WriteByte(Alphabet[n>>(60-(i*6))&0x3F])
 	}
-	/*
-		rb.WriteByte(Alphabet[n>>56&0x3F])
-		rb.WriteByte(Alphabet[n>>48&0x3F])
-		rb.WriteByte(Alphabet[n>>42&0x3F])
-		rb.WriteByte(Alphabet[n>>36&0x3F])
-		rb.WriteByte(Alphabet[n>>30&0x3F])
-		rb.WriteByte(Alphabet[n>>24&0x3F])
-		rb.WriteByte(Alphabet[n>>18&0x3F])
-		rb.WriteByte(Alphabet[n>>12&0x3F])
-		rb.WriteByte(Alphabet[n>>6&0x3F])
-		rb.WriteByte(Alphabet[n&0x3F]) // LSB
-	*/
 	return rb.String()
 }
 
 // DecodeUint64 returns a uint64 representing the sortablebase64-encoded string
 // s, or an error.
 func DecodeUint64(s string) uint64 {
-	var u uint64
 	if len(s) != 11 {
 		log.Fatalf("expected string of length 11, got string of length %d", len(s))
 	}
 	b := []byte(s)
+	var u uint64
 	for i := 0; i < 11; i++ {
 		v := uint64(decodeMap[b[i]])
 		if v == 0xFF {
 			log.Fatalf("illegal character at pos %d in '%s'", i, s)
 		}
-		shift := 60 - (6 * i)
-		shiftedV := v << shift
-		u = u | shiftedV
+		u |= v << (60 - (i * 6))
 	}
 	return u
 }

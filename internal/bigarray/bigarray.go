10b71391c5768a60af55d3509268dd7ffa00abd4
Vivek Sekhar <vivek@viveksekhar.ca>
2020-11-16 17:49:18 -0500

    Support batching.

--- internal/bigarray/bigarray.go
+++ internal/bigarray/bigarray.go
@@ -9,19 +9,51 @@ import (
 // Search uses binary search to find and return the smallest index i in
 // [min, maxInt] at which f(i) is true, assuming that on the range
 // [min, maxInt], f(i) == true implies f(i+1) == true. That is, Search requires
-// that f is false some (possibly empty) prefix of the input range [min, maxInt]
-// and then true thereafter.
-func Search(min int, f func(int) bool) int {
+// that f is false for some (possibly empty) prefix of the input range [min,
+// maxInt] and then true thereafter.
+func Search(min int, f func(i int) (atI bool)) int {
+	return SearchBatch(min, func(i int) (bool, int) {
+		return f(i), i
+	})
+}
+
+// SearchBatch is like Search, but f can return additional information to
+// improve performance. SearchBatch expects f to probe at index i and
+// sequentially zero or more additional indices > i, and return upon finding the
+// first index at which its condition evaluates to true.
+//
+// atLastChecked should be true if the condition is true at index lastChecked.
+//
+// lastChecked >= i is the last index SearchBatch checked.
+func SearchBatch(min int, f func(i int) (atLastChecked bool, lastChecked int)) int {
 	// Shift the search from [min, maxInt] to [0, maxInt-min]
-	g := func(i int) bool {
-		return f(min + i)
+	g := func(i int) (bool, int) {
+		t, lastChecked := f(min + i)
+		return t, lastChecked - min
 	}
 
 	// Exponentially find a reasonable upper bound.
-	high := 1
-	for !g(high) {
-		high *= 2
+	high := 0
+	for {
+		t, lastChecked := g(high)
+		if t {
+			// threshold is <= lastChecked, we found the value
+			if lastChecked > high {
+				return lastChecked + min
+			}
+			// threshold is < high, we're done finding an upper bound, jump to
+			// binary search between [0,high].
+			break
+		}
+		if high == 0 {
+			high = 1
+		} else {
+			high *= 2
+		}
+		if (lastChecked + 1) > high {
+			high = lastChecked + 1
+		}
 	}
 
-	return sort.Search(high, g) + min
+	return sort.Search(high, func(i int) bool { r, _ := g(i); return r }) + min
 }
--- internal/bigarray/bigarray_test.go
+++ internal/bigarray/bigarray_test.go
@@ -8,19 +8,19 @@ import (
 
 var cases [][]int = [][]int{
 	// next, min, expectedProbes
-	{0, 0, 2},
-	{0, 6, 2},
-	{100, 1, 15},
-	{4096, 100, 25},
-	{9999999999, 0, 69},
-	{9999999999, 9999999999, 2},
+	{0, 0, 1},
+	{0, 6, 1},
+	{100, 1, 16},
+	{4096, 100, 26},
+	{1e6, 0, 42},
+	{1e9, 0, 62},
+	{9999999999, 0, 70},
+	{9999999999, 9999999999, 1},
 }
 
 func TestSearch(t *testing.T) {
 	for _, c := range cases {
-		expectedNext := c[0]
-		min := c[1]
-		expectedProbes := c[2]
+		expectedNext, min, expectedProbes := c[0], c[1], c[2]
 		probes := 0
 		probe := func(i int) bool {
 			probes++
@@ -32,6 +32,25 @@ func TestSearch(t *testing.T) {
 		}
 		if next != expectedNext || probes != expectedProbes {
 			t.Errorf("expected %d (%d probes), got %d (%d probes)", expectedNext, expectedProbes, next, probes)
+		}
+	}
+}
+
+func TestSearchBatch(t *testing.T) {
+	for _, c := range cases {
+		expectedNext, min, _ := c[0], c[1], c[2]
+		probes := 0
+		batchProbe := func(i int) (bool, int) {
+			probes++
+			// pretend like we probe all the way up to expectedNext
+			return true, expectedNext
+		}
+		next := bigarray.SearchBatch(min, batchProbe)
+		if expectedNext < min {
+			expectedNext = min
+		}
+		if next != expectedNext || probes != 1 {
+			t.Errorf("expected %d (%d probes), got %d (%d probes)", expectedNext, 1, next, probes)
 		}
 	}
 }

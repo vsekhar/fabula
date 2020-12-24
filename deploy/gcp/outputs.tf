3de415632cf23556acc77feb946230519ae28017
Vivek Sekhar <vivek@viveksekhar.ca>
2020-12-23 20:02:53 -0500

    Emit GCE self link.

--- deploy/gcp/outputs.tf
+++ deploy/gcp/outputs.tf
@@ -1,7 +1,5 @@
 // TODO: server/endpoint URLs
 
-/*
-output "ip" {
-    value = module.fabula_kubernetes.kubernetes_load_balancer_ip
+output "self_link" {
+    value = "http://${google_compute_instance_from_template.fabula-instance.network_interface.0.access_config.0.nat_ip}"
 }
-*/
